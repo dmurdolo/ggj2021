945ba9c72bf2dc57fa1874d73ae1a48caa3c0168
benxtan <benxtan@gmail.com>
2021-01-30 06:13:41 +0000

    Added buildings and island from Kero Added more self driving cars

--- ggj2021project/Assets/Scripts/Managers/TrafficManager.cs
+++ ggj2021project/Assets/Scripts/Managers/TrafficManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class TrafficManager : MonoBehaviour
 {
+    // Assume left hand side of the road
+
     public GameObject[] Vehicles;
 
     private GameObject VehiclesParent;
@@ -13,20 +15,47 @@ public class TrafficManager : MonoBehaviour
     public void Init()
     {
         VehiclesParent = GameObject.Find("Vehicles");
-
-        PlaceVehicle(1, 1, -90);
+        
+        PlaceVehicle(1, 1, WorldManager.Direction.Right);
+        PlaceVehicle(23, 1, WorldManager.Direction.Down);
+        PlaceVehicle(23, 23, WorldManager.Direction.Left);
+        PlaceVehicle(1, 23, WorldManager.Direction.Up);
     }
 
     void Update()
     {
     }
 
-    private void PlaceVehicle(int x, int y, int direction)
+    private void PlaceVehicle(int x, int y, WorldManager.Direction direction)
     {
+        int yAngle = 0;
+        float xOffset = 0;
+        float yOffset = 0;
+
+        switch(direction)
+        {
+            case WorldManager.Direction.Up:
+                yAngle = 180;
+                break;
+            
+            case WorldManager.Direction.Down:
+                yAngle = 0;
+                break;
+            
+            case WorldManager.Direction.Left:
+                yAngle = 90;
+                break;
+            
+            case WorldManager.Direction.Right:
+                yAngle = 270;
+                //yOffset = -0.055f;
+                break;
+        }
+
         Vector2 worldPosition = WorldManager.GetTileWorldPosition(x, y);
         GameObject vehicle = Instantiate(Vehicles[Random.Range(0, Vehicles.Length)]);
         vehicle.transform.parent = VehiclesParent.transform;
-        vehicle.transform.eulerAngles = new Vector3(0, direction, 0);
-        vehicle.transform.position = new Vector3(worldPosition.x, 0.005f, worldPosition.y);
+        vehicle.transform.eulerAngles = new Vector3(0, yAngle, 0);
+        vehicle.transform.position = new Vector3(worldPosition.x + xOffset, 0.005f, worldPosition.y + yOffset);
     }
 }
